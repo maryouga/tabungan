84a8f5e977600847c7605039d99ec9a84988dd97
YDP <yogadwipriyanto@yahoo.com>
2025-05-30 13:05:27 +0700

    Initial commit: 6

--- src/main/java/com/tugas/spring/security/SecurityConfig.java
+++ src/main/java/com/tugas/spring/security/SecurityConfig.java
@@ -29,7 +29,10 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
